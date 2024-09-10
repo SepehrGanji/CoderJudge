d794a2ec6a1ee84007fcb92b8756019eb9a9d6d2
SepehrGanji <sepehrganji79@gmail.com>
2024-09-10 16:04:02 -0600

    receive messages from queue

--- app/src/main/java/com/coder/judge/App.java
+++ app/src/main/java/com/coder/judge/App.java
@@ -1,21 +1,32 @@
 package com.coder.judge;
 
-import java.sql.SQLException;
 
-import com.coder.judge.DB.DB;
+import org.apache.log4j.Logger;  
+
+import com.coder.judge.Queue.Queue;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
 
 public class App {
-
+    static Logger log = Logger.getLogger(App.class.getName());  
     public String getGreeting() {
+        log.info("testlog");
         return "Hello World!";
     }
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        try (var connection = DB.connect()) {
-            System.out.println("Connected to the PostgreSQL database.");
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
+        try {
+            Channel channel = Queue.getChannel();
+            //channel.queueDeclare("test", false, false, false, null);
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            };
+            channel.basicConsume("test", true, deliverCallback, consumerTag -> {
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
